17932d8094113c566b4b7ea19cc1d3c087c23792
Osiyannn <cube31394@gmail.com>
2024-09-19 20:52:47 +0545

    Add files via upload

---
+++ arthimetic operation.py
@@ -0,0 +1,17 @@
+x = 11
+y = 9
+
+print ('Sum: ', x + y)  
+
+print ('Subtraction: ', x - y)   
+
+print ('Multiplication: ', x * y)  
+
+print ('Division: ', x / y) 
+
+print ('Floor Division: ', x // y) #remember this 
+
+print ('Modulo: ', x % y)  
+
+print ('Power: ', x ** y)   
+
